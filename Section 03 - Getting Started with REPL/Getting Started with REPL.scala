1e8bfd296a8c4278cd705534c9136df2a0020239
romulovieira777 <romulo.vieira777@gmail.com>
2021-06-15 12:24:06 -0300

    Getting Started with REPL

---
+++ Section 03 - Getting Started with REPL/Getting Started with REPL.scala
@@ -0,0 +1,132 @@
+scala> 5
+res0: Int = 5
+
+scala> 10 + 5
+res1: Int = 15
+
+scala> 10 * 1000099999
+res2: Int = 1411065398
+
+scala> "Hello"
+res3: String = Hello
+
+scala> "Hello " + "World!"
+res4: String = Hello World!
+
+cala> 5
+res0: Int = 5
+
+scala> res0
+res1: Int = 5
+
+scala> res1 * 100
+res2: Int = 500
+
+scala> var num = 99
+num: Int = 99
+
+scala> 1.9
+res3: Double = 1.9
+
+scala> true
+res4: Boolean = true
+
+scala> "Hello"
+res5: String = Hello
+
+scala> res5 + 100
+res6: String = Hello100
+
+scala> res5 + res3
+res7: String = Hello1.9
+
+scala> "hello"
+res0: String = hello
+
+scala> res0
+res1: String = hello
+
+scala> res0.toUpperCase()
+res2: String = HELLO
+
+scala> res0.length()
+res3: Int = 5
+
+scala> var a1 = 100
+a1: Int = 100
+
+scala> var b1 = 200
+b1: Int = 200
+
+scala> var c1 = a1 * b1
+c1: Int = 20000
+
+scala> :paste
+// Entering paste mode (ctrl-D to finish)
+
+var a2 = 100
+var b2 = 200
+var c2 = a2 * b2
+
+// Exiting paste mode, now interpreting.
+
+a2: Int = 100
+b2: Int = 200
+c2: Int = 20000
+
+scala> var lunch = 123.45 / 4
+lunch: Double = 30.8625
+
+scala> var coffee = 50.67 / 4
+coffee: Double = 12.6675
+
+scala> var photoshoot = 87.76 / 4
+photoshoot: Double = 21.94
+
+scala> var taxis = 77.12 / 4
+taxis: Double = 19.28
+
+scala> var totalPerPerson = lunch + coffee + photoshoot + taxis
+totalPerPerson: Double = 84.75
+
+scala> :he replay
+
+reset the repl and replay all previous commands
+
+scala> :replay
+Replaying: "hello"
+res0: String = hello
+
+Replaying: res0
+res1: String = hello
+
+Replaying: res0.toUpperCase()
+res2: String = HELLO
+
+Replaying: res0.length()
+res3: Int = 5
+
+Replaying: var a1 = 100
+a1: Int = 100
+
+Replaying: var b1 = 200
+b1: Int = 200
+
+Replaying: var c1 = a1 * b1
+c1: Int = 20000
+
+Replaying: var lunch = 123.45 / 4
+lunch: Double = 30.8625
+
+Replaying: var coffee = 50.67 / 4
+coffee: Double = 12.6675
+
+Replaying: var photoshoot = 87.76 / 4
+photoshoot: Double = 21.94
+
+Replaying: var taxis = 77.12 / 4
+taxis: Double = 19.28
+
+Replaying: var totalPerPerson = lunch + coffee + photoshoot + taxis
+totalPerPerson: Double = 84.75
+
