54178556ddb574d96784600399e92a3ad5f057e4
romulovieira777 <romulo.vieira777@gmail.com>
2021-06-15 12:49:15 -0300

    Working with Variables and Basic Types

---
+++ Section 04 - Working with Variables and Basic Types/Understanding_Variables_in_Scala.scala
@@ -0,0 +1,46 @@
+scala> var number_01: Int = 10
+number_01: Int = 10
+
+scala> var number_02: Int = 20
+number_02: Int = 20
+
+scala> number_01 = -30
+number_01: Int = -30
+
+scala> number_01
+res0: Int = -30
+
+scala> val number_03: Int = 45
+number_03: Int = 45
+
+scala> number_03
+res1: Int = 45
+
+scala> val number = 56 // this is a special number
+number: Int = 56
+
+scala> number
+res2: Int = 56
+
+scala> :paste
+// Entering paste mode (ctrl-D to finish)
+
+/**
+ * This is 
+ * a multiline
+ * comment
+ */
+val number_05: Int = 99
+
+// Exiting paste mode, now interpreting.
+
+number_05: Int = 99
+
+scala> number_05
+res3: Int = 99
+
+scala> var aString: String = "He said"
+aString: String = He said
+
+scala> var aString: String = "He said - \"I am good, how are you?\""
+aString: String = He said - "I am good, how are you?"
